1463633cfcc8aefa6c497e8f6bd1b570e45ee498
tuchui <maoqingsingle@163.com>
2019-04-18 17:31:44 +0800

    json 转换器测试

--- src/main/java/com/mao/controller/TestController.java
+++ src/main/java/com/mao/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.mao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 /**
  * @author:mao
@@ -15,12 +17,42 @@ public class TestController {
 
     @RequestMapping("/hello")
     public String hello(){
+
         return "test";
     }
 
     @RequestMapping("/{hello}")
     @ResponseBody
-    public String test(@PathVariable("hello") String path){
-        return path;
+    public Student test(@PathVariable("hello") String path){
+        Student s=new Student();
+        s.setAge(22);
+        s.setName("jack");
+        return s;
+    }
+
+    @RequestMapping("/studeng")
+    public String student(Model model){
+       model.addAttribute("studeng","test");
+       return "jsonView";
+    }
+}
+class Student{
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
